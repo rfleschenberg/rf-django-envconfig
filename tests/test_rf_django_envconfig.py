5a1d23aafa461e96838f4e922da143a7de6d5347
René Fleschenberg <rene@fleschenberg.net>
2016-12-14 21:51:07 +0100

    Introduce Hypothesis

--- tests/test_rf_django_envconfig.py
+++ tests/test_rf_django_envconfig.py
@@ -1,20 +1,54 @@
+from hypothesis import given, assume
+import hypothesis.strategies as st
+
 from rf_django_envconfig import get_bool
 
 
-def test_get_bool():
-    false_values = [
-        'False',
-        'false',
-        'Off',
-        'off',
-        '0',
-        'No',
-        'no',
-        '',
-    ]
-    for value in false_values:
-        env = {'test': value}
-        assert not get_bool(env, 'test')
-    assert get_bool({}, 'nosuchvar', default=True)
-    assert not get_bool({}, 'nosuchvar')
-    assert get_bool({'myvar': '1'}, 'myvar')
+FALSE_VALUES = [
+    'False',
+    'false',
+    'Off',
+    'off',
+    '0',
+    'No',
+    'no',
+    '',
+]
+
+
+@given(x=st.text())
+def test_get_bool_true(x):
+    assume(x not in FALSE_VALUES)
+    env = {'test': x}
+    assert get_bool(env, 'test')
+
+
+@given(x=st.text())
+def test_get_bool_false(x):
+    assume(x in FALSE_VALUES)
+    env = {'test': x}
+    assert not get_bool(env, 'test')
+
+
+@given(x=st.text())
+def test_get_bool_default_true_value_absent(x):
+    assert get_bool({}, 'test', default=True)
+
+
+@given(x=st.text())
+def test_get_bool_default_false_value_absent(x):
+    assert not get_bool({}, 'test',  default=False)
+
+
+@given(x=st.text())
+def test_get_bool_default_true_value_present(x):
+    assume(x in FALSE_VALUES)
+    env = {'test': x}
+    assert not get_bool(env, 'test',  default=True)
+
+
+@given(x=st.text())
+def test_get_bool_default_false_value_present(x):
+    assume(x not in FALSE_VALUES)
+    env = {'test': x}
+    assert get_bool(env, 'test',  default=False)
