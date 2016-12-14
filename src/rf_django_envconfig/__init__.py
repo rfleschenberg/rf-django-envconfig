28c0e47cadc8a1ba4bd06ce26f6fed355cb39232
René Fleschenberg <rene@fleschenberg.net>
2016-12-14 20:56:19 +0100

    Add a function to get booleans from the environment

--- src/rf_django_envconfig/__init__.py
+++ src/rf_django_envconfig/__init__.py
@@ -1,1 +1,28 @@
 __version__ = "0.1.0"
+
+
+def get_bool(env, name, default=False):
+    """Get a boolean value from the environment
+
+    If the value is not set in the environment, return ``True`` if ``default`` evaluates to
+    ``True``, otherwise return ``False``.
+
+    The following values are considered ``False``:
+
+        * 'False'
+        * 'false'
+        * 'Off'
+        * 'off'
+        * '0'
+        * 'No'
+        * 'no'
+        * ''
+
+    Any other value is considered ``True``.
+    """
+    if name not in env:
+        return bool(default)
+    value = env[name]
+    if value in ['False', 'false', 'Off', 'off', '0', 'No', 'no', '']:
+        return False
+    return True
--- tests/test_rf_django_envconfig.py
+++ tests/test_rf_django_envconfig.py
@@ -1,6 +1,20 @@
+from rf_django_envconfig import get_bool
 
-import rf_django_envconfig
 
-
-def test_main():
-    assert rf_django_envconfig  # use your library here
+def test_get_bool():
+    false_values = [
+        'False',
+        'false',
+        'Off',
+        'off',
+        '0',
+        'No',
+        'no',
+        '',
+    ]
+    for value in false_values:
+        env = {'test': value}
+        assert not get_bool(env, 'test')
+    assert get_bool({}, 'nosuchvar', default=True)
+    assert not get_bool({}, 'nosuchvar')
+    assert get_bool({'myvar': '1'}, 'myvar')
