59b460e0b01b154343d77154d6f9b260bbc11e19
René Fleschenberg <rene@fleschenberg.net>
2016-12-15 00:00:37 +0100

    Add get_list function

--- src/rf_django_envconfig/__init__.py
+++ src/rf_django_envconfig/__init__.py
@@ -26,3 +26,21 @@ def get_bool(env, name, default=False):
     if value in ['False', 'false', 'Off', 'off', '0', 'No', 'no', '']:
         return False
     return True
+
+
+def get_list(env, name, default=None):
+    """Get a list from the environment
+
+    The input is assumed to be a comma-separated list of strings.
+
+    If ``name`` is not present in the environment, return ``default``. Note
+    that ``default`` is returned as-is, so you should usually specify it as a
+    list::
+
+        ALLOWED_HOSTS = get_list(os.environ, 'ALLOWED_HOSTS', ['localhost', ])
+    """
+    if default is None:
+        default = []
+    if name not in env:
+        return default
+    return env[name].split(',')
--- tests/test_rf_django_envconfig.py
+++ tests/test_rf_django_envconfig.py
@@ -3,6 +3,7 @@ from hypothesis import assume
 from hypothesis import given
 
 from rf_django_envconfig import get_bool
+from rf_django_envconfig import get_list
 
 
 FALSE_VALUES = [
@@ -15,6 +16,12 @@ FALSE_VALUES = [
     'no',
     '',
 ]
+
+
+@st.composite
+def comma_separated_values(draw, elements=st.text()):
+    xs = draw(st.lists(elements, average_size=10))
+    return ','.join(xs)
 
 
 @given(x=st.text())
@@ -53,3 +60,19 @@ def test_get_bool_default_false_value_present(x):
     assume(x not in FALSE_VALUES)
     env = {'test': x}
     assert get_bool(env, 'test',  default=False)
+
+
+@given(x=comma_separated_values())
+def test_get_list(x):
+    items = x.split(',')
+    assert get_list({'test': x}, 'test') == items
+
+
+@given(x=comma_separated_values())
+def test_get_list_value_absent(x):
+    assert get_list({}, 'test') == []
+
+
+@given(x=comma_separated_values())
+def test_get_list_value_absent_with_default(x):
+    assert get_list({}, 'test', ['localhost', ]) == ['localhost', ]
